9f21c26dd5b04b2424726ea43c4c9880f7fc059e
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2016-09-16 17:17:48 -0300

    Pagina de agradecimiento

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,3 +1,5 @@
+# import unittest
+
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
@@ -44,8 +46,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -77,8 +79,9 @@ class SubscribePostInvalid(TestCase):
         self.assertFalse(Subscription.objects.exists())
 
 
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Ramiro Alvaro', cpf='12345678901', email='ramiroalvaro.ra@gmail.com', phone='31-991387178')
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
+# @unittest.skip('To be removed.')
+# class SubscribeSuccessMessage(TestCase):
+#     def test_message(self):
+#         data = dict(name='Ramiro Alvaro', cpf='12345678901', email='ramiroalvaro.ra@gmail.com', phone='31-991387178')
+#         response = self.client.post('/inscricao/', data, follow=True)
+#         self.assertContains(response, 'Inscrição realizada com sucesso!')
